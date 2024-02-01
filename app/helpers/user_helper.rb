1f2d344dc831b6216f87163ea29f968facdecc7a
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-02-01 14:04:32 +0100

    base fonctionnelle comments

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,21 +1,15 @@
 module UserHelper
-    def user_name(user)
-        athlete = Athlete.find_by(user: user.id)
-        fan = Fan.find_by(user: user.id)
-      
-        if athlete
-          athlete.name
-        elsif fan
-          fan.pseudo
-        else
-          # Default name or handle the case when neither athlete nor fan is found
-          "Unknown User"
-        end
-      end
-      
+    def user_info(user)
+      athlete = Athlete.find_by(user: user.id)
+      fan = Fan.find_by(user: user.id)
+    
 
-    def user_type_path
-        #permet de faire le lien vers fan ou athlete path en fonction du type
-        #co,struire path sur meme model que name 
-    end
+      if athlete
+        { name: athlete.name, path: athlete_path(athlete) }
+      elsif fan
+        { name: fan.pseudo, path: fan_path(fan) }
+      else
+        { name: "Unknown User", path: nil }
+      end
+  end
 end
