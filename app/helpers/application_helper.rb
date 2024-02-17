9b881c778f79e407c96432a8fd762f75c0da73c5
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-02-17 18:06:04 +0100

    footer avec helper pour style de btns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def active_link_colored(link_path, text, icon, controller, action)
+          
+        css_classes = (controller_name == controller && (action.nil? || action_name == action)) ? ' orange-colored' : 'text-secondary'
+  
+        link_to(content_tag(:div, class: "btn btn-link #{css_classes}") do
+          concat content_tag(:i, "", class: "fa fa-#{icon}")
+          concat content_tag(:div, text, class: "")
+        end, link_path)
+
+    end
 end
